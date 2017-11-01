7d529cb32a3efa73e56d0ba287a1c02c049a6c8d
Alina Shakya <alina.shakya3@gmail.com>
2017-11-01 18:05:41 +0000

    #master - changes in execution state

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ class App extends Component {
 				videos : [],
 				selectedVideo : null
 			};
-	this.videoSearch('surfboards');
+	this.videoSearch('artificial intelligence');
 	}
 
 	videoSearch(term){
@@ -30,7 +30,7 @@ class App extends Component {
 	}
 
 	render() {
-		const videoSearch = _.debounce((term) => {this.videoSearch(term)},1000);
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)},500);
 
         return (
                 <div>
